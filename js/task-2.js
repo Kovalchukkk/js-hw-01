439a36861cd8a38bf822e3218bba8a0716ab1a8a
Mykola Kovalchuk <kolyankov@gmail.com>
2022-06-19 14:51:10 +0300

    add task-2

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+const total = 100;
+const ordered = 50;
+
+const res = ordered > total ? `На складі недостатньо товарів!` : `Замовлення оформлено, з вами зв'яжеться менеджер.`;
+
+console.log(res);
+
